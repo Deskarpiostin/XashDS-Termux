44850e7b6f7b86aa1df7e0c0c0fa4ca727de9212
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-04-29 04:21:57 +0800

    Update setup.sh
    deleted symlink creating
    changed echo text

--- files/setup.sh
+++ files/setup.sh
@@ -5,14 +5,15 @@ echo "This is setup for your Xash3D server running on Termux, this might take a 
 # proot-distro
 pkg install proot-distro -y > /dev/null 2>&1
 
+echo "Installing Debian (1/2 command are processed right now)"
+
 # debian installation
 proot-distro install debian > /dev/null 2>&1
 
-# create a symlink for debian
-proot-distro login debian > /dev/null 2>&1
-ln -s /storage/emulated/0/xashds/ ~/xashds > /dev/null 2>&1
+echo "Copying XashDS folder (2/2 command are processed right now)
 
-# xashds folder
+# create a copy of xashds for debian
+proot-distro login debian > /dev/null 2>&1
 cp -r /storage/emulated/0/xashds/ ~/xashds > /dev/null 2>&1
 
 echo "Setup process is done! Now follow the next commands in your browser."
