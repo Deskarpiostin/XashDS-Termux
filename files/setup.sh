7b85a148781ade851bcc276da8929898467e6f54
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-05-01 23:46:06 +0800

    Update setup.sh
    reverted changes from ru.sh

--- files/setup.sh
+++ files/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "This is setup for your Xash3D server running on Termux, this might take a while.."
+echo "Installing proot-distro (1/1 command are processed right now)"
 
 # proot-distro
 pkg install proot-distro -y > /dev/null 2>&1
@@ -10,7 +11,7 @@ echo "Installing Debian (1/2 command are processed right now)"
 # debian installation
 proot-distro install debian > /dev/null 2>&1
 
-echo "Copying XashDS folder (2/2 command are processed right now)"
+echo "Copying XashDS folder (1/3 command are processed right now)"
 
 # create a copy of xashds for debian
 proot-distro login debian > /dev/null 2>&1
