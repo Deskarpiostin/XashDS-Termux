cd8fd7a0c8b9571bbf74410ac48fd4f17a46ea01
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-05-20 22:03:01 +0800

    Update ru.sh

--- files/ru.sh
+++ files/ru.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Это настроит твой Xash3D сервер на Termux, может занять время.."
-echo "Установка proot-distro (1/1 команда в процессе сейчас)"
+echo "Установка proot-distro (1/3 команда в процессе сейчас)"
 
 # proot-distro
 pkg install proot-distro -y > /dev/null 2>&1
 
-echo "Установка Debian (1/2 команда в процессе сейчас)"
+echo "Установка Debian (2/3 команда в процессе сейчас)"
 
 # debian installation
 proot-distro install debian > /dev/null 2>&1
