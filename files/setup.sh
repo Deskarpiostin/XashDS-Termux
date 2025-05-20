f12827b97bc2d7b70175fd7a876fd7f85e756bcb
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-05-20 22:02:28 +0800

    Update setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 echo "This is setup for your Xash3D server running on Termux, this might take a while.."
-echo "Installing proot-distro (1/1 command are processed right now)"
+echo "Installing proot-distro (1/3 command are processed right now)"
 
 # proot-distro
 pkg install proot-distro -y > /dev/null 2>&1
 
-echo "Installing Debian (1/2 command are processed right now)"
+echo "Installing Debian (2/3 command are processed right now)"
 
 # debian installation
 proot-distro install debian > /dev/null 2>&1
 
-echo "Copying XashDS folder (1/3 command are processed right now)"
+echo "Copying XashDS folder (3/3 command are processed right now)"
 
 # create a copy of xashds for debian
 proot-distro login debian > /dev/null 2>&1
