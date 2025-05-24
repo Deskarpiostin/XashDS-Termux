9aecfb3b57767dc93a43f0d5c75150beab8ceda6
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-05-25 00:42:16 +0800

    Update ru.sh

--- files/ru.sh
+++ files/ru.sh
@@ -4,7 +4,7 @@ echo "Это настроит твой Xash3D сервер на Termux, може
 echo "Установка поддержки amd64 (спасибо mcagabe19, 1/4 команда в процессе сейчас"
 
 #amd64
-apt install git qemu-user-x86-64 blink proot-distro -y && git clone https://github.com/mcagabe19-stuff/termux-proot-x86 && cd termux-proot-x86 && bash ./movedistrosx86_64.sh
+apt install git qemu-user-x86-64 blink proot-distro -y && git clone https://github.com/mcagabe19-stuff/termux-proot-x86 && cd termux-proot-x86 && bash ./movedistrosx86_64.sh > /dev/null 2>&1
 
 echo "Установка proot-distro (2/4 команда в процессе сейчас)"
 
