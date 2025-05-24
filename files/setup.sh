1adbc0f851e6ff04b800ac894aa99d559babb0c4
Deskarpiostin <georgijcerenkov074@gmail.com>
2025-05-25 00:41:59 +0800

    Update setup.sh

--- files/setup.sh
+++ files/setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 echo "This is setup for your Xash3D server running on Termux, this might take a while.."
-echo "Installing proot-distro (1/3 command are processed right now)"
+echo "Installing x86_64 support (thanks mcagabe19!, 1/4 command are processed right now)"
+
+# amd64
+apt install git qemu-user-x86-64 blink proot-distro -y && git clone https://github.com/mcagabe19-stuff/termux-proot-x86 && cd termux-proot-x86 && bash ./movedistrosx86_64.sh > /dev/null 2>&1
+
+echo "Installing proot-distro (2/4 command are processed right now)"
 
 # proot-distro
 pkg install proot-distro -y > /dev/null 2>&1
 
-echo "Installing Debian (2/3 command are processed right now)"
+echo "Installing Debian (3/4 command are processed right now)"
 
 # debian installation
 proot-distro install debian > /dev/null 2>&1
 
-echo "Copying XashDS folder (3/3 command are processed right now)"
+echo "Copying XashDS folder (4/4 command are processed right now)"
 
 # create a copy of xashds for debian
 proot-distro login debian > /dev/null 2>&1
